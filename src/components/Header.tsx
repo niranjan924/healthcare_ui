9ff5dd10795539a0c0d45b8bb874585391ea27c8
niranjan924 <niranjan.salihundam@gmail.com>
2025-09-05 13:03:28 +0530

    contact uncomment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -56,13 +56,13 @@ export default function Header() {
               className="text-gray-700 hover:text-blue-600 font-medium transition-colors"
             >
               Blog
-            </Link>
+            </Link> */}
             <Link
               href="/contact"
               className="text-gray-700 hover:text-blue-600 font-medium transition-colors"
             >
               Contact
-            </Link> */}
+            </Link>
             <button className="bg-gradient-to-r from-cyan-700 via-cyan-600 to-emerald-600 text-white px-4 py-2 rounded-md">
               Book Appointment
             </button>
@@ -114,13 +114,13 @@ export default function Header() {
                 className="text-gray-700 hover:text-blue-600 font-medium"
               >
                 Blog
-              </Link>
+              </Link> */}
               <Link
                 href="/contact"
                 className="text-gray-700 hover:text-blue-600 font-medium"
               >
                 Contact
-              </Link> */}
+              </Link>
               <button className="bg-gradient-to-r from-cyan-700 via-cyan-600 to-emerald-600 text-white w-full px-4 py-2 rounded-md">
                 Book Appointment
               </button>
